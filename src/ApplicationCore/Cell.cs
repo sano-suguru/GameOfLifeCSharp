4fe1931d176cf89e552a5c298e9bf93f85d15dff
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2023-03-07 01:19:33 +0900

    リファクタリング

--- src/ApplicationCore/Cell.cs
+++ src/ApplicationCore/Cell.cs
@@ -4,7 +4,7 @@ public struct Cell
 {
     public int X { get; }
     public int Y { get; }
-    public bool IsAlive { get; private set; }
+    public bool IsAlive { get; }
 
     public Cell((int x, int y) point, bool isAlive)
     {
